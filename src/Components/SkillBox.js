088398de9a017770b76f94ea6352636036de58ce
TaeWooKim-SCH <zop1234@hanmail.net>
2023-08-30 20:20:33 +0900

    [fix] 스킬 카드 호버시 늘어나도록 변경

--- src/Components/SkillBox.js
+++ src/Components/SkillBox.js
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import './SkillBox.css';
 
 export default function AboutSkill({svg, title, text}) {
+  const [isOver, setIsOver] = useState(false);
+
+  const onMouseOverHandler = () => {
+    setIsOver((prev) => !prev);
+  }
+
   return (
-    <section className="skill-box" data-aos = "fade-up" data-aos-duration = "800">
-      <img src={svg} alt="html" style={{width:'70px'}}></img>
-      <section className="skill-box-right">
-        <div className="skill-title">{title}</div>
-        <div className="skill-content">{text}</div>
+    <main className="skill-main">
+      <section
+        className="skill-box"
+        data-aos="fade-up"
+        data-aos-duration="800"
+        onMouseEnter={onMouseOverHandler}
+        onMouseLeave={onMouseOverHandler}
+      >
+        <img className="skill-img" src={svg} alt="html" style={{width:'70px'}}></img>
+        {isOver && (
+          <section className="skill-box-right">
+            <div className="skill-title">{title}</div>
+            <div className="skill-content">{text}</div>
+          </section>
+        )}
       </section>
-    </section>
+    </main>
   );
 }
