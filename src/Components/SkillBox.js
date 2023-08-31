777c1282f7689d1964564bbbfcd145bc82f80dbe
TaeWooKim-SCH <zop1234@hanmail.net>
2023-08-31 13:41:34 +0900

    [feat] 스킬 박스 호버시 늘어나도록 성공

--- src/Components/SkillBox.js
+++ src/Components/SkillBox.js
@@ -1,30 +1,19 @@
-import React, { useState } from "react";
+import React from "react";
 import './SkillBox.css';
 
 export default function AboutSkill({svg, title, text}) {
-  const [isOver, setIsOver] = useState(false);
-
-  const onMouseOverHandler = () => {
-    setIsOver((prev) => !prev);
-  }
 
   return (
-    <main className="skill-main">
-      <section
-        className="skill-box"
-        data-aos="fade-up"
-        data-aos-duration="800"
-        onMouseEnter={onMouseOverHandler}
-        onMouseLeave={onMouseOverHandler}
-      >
-        <img className="skill-img" src={svg} alt="html" style={{width:'70px'}}></img>
-        {isOver && (
-          <section className="skill-box-right">
-            <div className="skill-title">{title}</div>
-            <div className="skill-content">{text}</div>
-          </section>
-        )}
-      </section>
-    </main>
+    <section
+      className="skill-box"
+      data-aos="fade-up"
+      data-aos-duration="500"
+    >
+      <img className="skill-img" src={svg} alt="html"></img>
+        <section className="skill-box-right">
+          <div className="skill-title">{title}</div>
+          <div className="skill-content">{text}</div>
+        </section>
+    </section>
   );
 }
--- src/Page/Skills.js
+++ src/Page/Skills.js
@@ -11,6 +11,7 @@ export default function Skills() {
   return(
     <section id="skills">
       <div className="skills-title">SKILLS</div>
+      <div className="notice-content">아이콘 위에 마우스를 올리면 자세한 설명이 나옵니다.</div>
       <div className="skills-kind"># Front End</div>
       <section className="front-skills">
         {frontEnd.map((x) => (
