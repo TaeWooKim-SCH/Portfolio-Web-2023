015366d16341c1a8916cd8ce52684e536923dc64
TaeWooKim-SCH <zop1234@hanmail.net>
2023-08-31 21:34:45 +0900

    [fix] 프로젝트 디자인 수정

--- src/Components/ProjectCard.js
+++ src/Components/ProjectCard.js
@@ -1,16 +1,38 @@
 import React from "react";
-import Tag from "./Tag";
 
 export default function ProjectCard({url, title, kind, comment, tags}) {
   return (
-    <section className='projects-main-mid'>
+    <main className='projects-main-mid'>
       <img className="project-img" alt="프로젝트 이미지" src={url}></img>
-      <div className="project-title">{title}</div>
+      <section className="project-description">
+        <article className="project-description-inline">
+          <div>{title}</div>
+          <div>{kind}</div>
+        </article>
+      </section>
+      {/* <div className="project-title">{title}</div>
       <div className="project-kind">{kind}</div>
       <div className="project-comment">{comment}</div>
       <div className='tag-box'>
         {tags.map((tag) => <Tag word={tag} key={tag} />)}
-      </div>
-    </section>
+      </div> */}
+    </main>
   );
 }
+
+// import React from "react";
+// import Tag from "./Tag";
+
+// export default function ProjectCard({url, title, kind, comment, tags}) {
+//   return (
+//     <section className='projects-main-mid'>
+//       <img className="project-img" alt="프로젝트 이미지" src={url}></img>
+//       <div className="project-title">{title}</div>
+//       <div className="project-kind">{kind}</div>
+//       <div className="project-comment">{comment}</div>
+//       <div className='tag-box'>
+//         {tags.map((tag) => <Tag word={tag} key={tag} />)}
+//       </div>
+//     </section>
+//   );
+// }
--- src/Page/Projects.js
+++ src/Page/Projects.js
@@ -1,29 +1,18 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './Projects.css'
-import {IoIosArrowBack, IoIosArrowForward} from 'react-icons/io';
 import ProjectCard from '../Components/ProjectCard';
 import ProjectsData from '../data/ProjectsData';
 
 export default function Projects() {
-  const [page, setPage] = useState(0);
-
-  const arrowClickHandler = (type) => {
-    if (type === "left") {
-      if (page === 0) return setPage(ProjectsData.length - 1);
-      setPage((prev) => prev - 1);
-    }
-    else {
-      if (page === ProjectsData.length - 1) return setPage(0);
-      setPage((prev) => prev + 1);
-    }
-  }
 
   return(
-    <section id="projects">
-      <div className='projects-title'>PROJECTS</div>
-      <main className="projects-main">
-        {ProjectsData.map((project, index) => (
-          <div className={`${!(page === index) && 'hidden'}`} key={index}>
+    <main id="projects">
+      <section className='projects-title'>PROJECTS</section>
+      <section className="projects-bottom">
+        {/* {ProjectsData.length > 9 && <section className="more-view-link">더보기 {'>'}</section>} */}
+        <section className="more-view-link">더보기 {'>'}</section>
+        <section className="projects-main">
+          {ProjectsData.map((project, index) => (
             <ProjectCard
               url={project.imgUrl}
               title={project.title}
@@ -32,11 +21,53 @@ export default function Projects() {
               tags={project.tags}
               key={project.title}
             />
-          </div>
-        ))}
-      </main>
-      <IoIosArrowBack className="arrow arrow-left" size="100" fill='#6D6D6D' onClick={() => arrowClickHandler('left')} />
-      <IoIosArrowForward className="arrow arrow-right" size="100" fill='#6D6D6D' onClick={() => arrowClickHandler('right')} />
-    </section>
+          ))}
+        </section>
+      </section>
+    </main>
   );
 }
+
+
+// import React, {useState} from 'react';
+// import './Projects.css'
+// import {IoIosArrowBack, IoIosArrowForward} from 'react-icons/io';
+// import ProjectCard from '../Components/ProjectCard';
+// import ProjectsData from '../data/ProjectsData';
+
+// export default function Projects() {
+//   const [page, setPage] = useState(0);
+
+//   const arrowClickHandler = (type) => {
+//     if (type === "left") {
+//       if (page === 0) return setPage(ProjectsData.length - 1);
+//       setPage((prev) => prev - 1);
+//     }
+//     else {
+//       if (page === ProjectsData.length - 1) return setPage(0);
+//       setPage((prev) => prev + 1);
+//     }
+//   }
+
+//   return(
+//     <section id="projects">
+//       <div className='projects-title'>PROJECTS</div>
+//       <main className="projects-main">
+//         {ProjectsData.map((project, index) => (
+//           <div className={`${!(page === index) && 'hidden'}`} key={index}>
+//             <ProjectCard
+//               url={project.imgUrl}
+//               title={project.title}
+//               kind={project.kind}
+//               comment={project.comment}
+//               tags={project.tags}
+//               key={project.title}
+//             />
+//           </div>
+//         ))}
+//       </main>
+//       <IoIosArrowBack className="arrow arrow-left" size="100" fill='#6D6D6D' onClick={() => arrowClickHandler('left')} />
+//       <IoIosArrowForward className="arrow arrow-right" size="100" fill='#6D6D6D' onClick={() => arrowClickHandler('right')} />
+//     </section>
+//   );
+// }
