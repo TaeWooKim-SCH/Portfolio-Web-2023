f87846581646891bf775f4f309a8341e7fb08aca
TaeWooKim-SCH <zop1234@hanmail.net>
2023-09-04 23:10:05 +0900

    [feat] 프로젝트 카드 완료

--- src/Components/ProjectCard.js
+++ src/Components/ProjectCard.js
@@ -1,19 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function ProjectCard({projectInfo}) {
   return (
-    <main className='project-card-container'>
-      <section className="project-card-left-section">
-        <section className="project-card-left-top-section">
-          <div className="project-title">{projectInfo.title}</div>
-          <div className="project-period">{projectInfo.kind}, {projectInfo.period}</div>
+    <Link to={`projects/${projectInfo.title}`} style={{textDecoration: 'none'}}>
+      <main className='project-card-container'>
+        <section className="project-card-left-section">
+          <section className="project-card-left-top-section">
+            <div className="project-title">{projectInfo.title}</div>
+            <div className="project-period">{projectInfo.kind}, {projectInfo.period}</div>
+          </section>
+          <section className='project-card-tag-box'>
+            {projectInfo.tags.map((tag) => <div className="project-card-tag" key={tag}># {tag}</div>)}
+          </section>
+          <div className="project-card-intro">{projectInfo.intro}</div>
         </section>
-        <section className='project-card-tag-box'>
-          {projectInfo.tags.map((tag) => <div className="project-card-tag" key={tag}># {tag}</div>)}
-        </section>
-        <div className="project-card-intro">{projectInfo.intro}</div>
-      </section>
-      <img className="project-img" alt="프로젝트 이미지" src={projectInfo.imgUrl}></img>
-    </main>
+        <img className="project-img" alt="프로젝트 이미지" src={projectInfo.imgUrl}></img>
+      </main>
+    </Link>
   );
 }
