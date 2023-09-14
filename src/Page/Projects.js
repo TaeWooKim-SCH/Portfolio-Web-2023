592c5e19d47e7aec7b2e0e895cc707e7b0daa3c0
TaeWooKim-SCH <zop1234@hanmail.net>
2023-09-14 16:15:15 +0900

    feat: 프로젝트 코멘트 추가

--- src/Page/Projects.js
+++ src/Page/Projects.js
@@ -7,6 +7,7 @@ export default function Projects() {
   return(
     <main id="projects">
       <section className='projects-title'>PROJECTS</section>
+      <div className="project-notice-content">카드를 클릭하면 자세한 설명을 볼 수 있습니다.</div>
       <section className="projects-bottom">
         <section className="projects-main">
           {ProjectsData.map((project, idx) => (
